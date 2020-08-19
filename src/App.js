db3c2c074b0b65112df8f8183cdf2c0143148b8a
Adrien Charles <acharles@axxio.io>
2020-08-19 14:48:17 +0100

    AdminLTE-Integrated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+
+import Header from "./Header";
+import Menu from "./Menu";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header/>
+        <Menu/>
+        <MainContent/>
+        <Footer/>
       </div>
-    );
+  )
+
   }
+
 }
+
+
+
 
 export default withAuthenticator(App, true);
---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="main-footer">
+                    <strong>
+                        Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.
+                    </strong>
+                    All rights reserved.
+                    <div className="float-right d-none d-sm-inline-block">
+                        <b>Version</b> 3.0.5
+                    </div>
+                </footer>;
+
+            </div>
+        );
+    }
+}
+
+export default Footer;
